e9ed016367336b4efd643803375c056f936c0644
firmo holanda <firmo@viize.com>
2020-02-18 17:25:05 -0300

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to root_path, flash: { success: 'You are logged out.' }
+    redirect_to root_path
+    flash[:success] = 'You are logged out.'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,34 +26,29 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'accout created, welcome!'
+      log_in(@user)
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
+
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user
+      flash[:notice] = 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url
+    flash.now[:notice] = 'User was successfully destroyed.'
   end
 
   private
