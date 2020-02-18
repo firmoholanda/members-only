5da21831ef4faed69af42600eae73153c9ff2ea5
firmo holanda <firmo@viize.com>
2020-02-18 17:14:18 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ApplicationRecord
   # Returns the hash digest of the given string.
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost unless BCrypt::Engine::MIN_COST
-  
+
     BCrypt::Engine.cost
-    
+
     BCrypt::Password.create(string, cost: cost)
   end
 
